eaf6831d28833a3ff9be0f8c2cb2160f389d4302
unknown <muhammadaaliyan39@gmail.com>
2025-06-24 00:44:54 -0700

    some changes

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.listen(PORT, () => {
 
 //Create New Post
 app.post("/create-post", (req, res) => {
-  const { userId, text, image = "" } = req.body;
+  const { userId, text = "", image = "" } = req.body;
 
   if (!userId || !text) {
     return res.status(400).json({ message: "userId and text are required" });
